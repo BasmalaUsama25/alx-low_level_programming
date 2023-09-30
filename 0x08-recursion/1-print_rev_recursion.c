01f4cc6e62433b10fd3b78b00423df311f87451d
basmala <ubasmala25@gmail.com>
2023-09-30 23:07:41 +0300

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * _print_rev_recursion - prints a function in reverse
+ *
+ * @s: pointer
+ *
+ * Return: Always 1 (success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+    if (*s == 1)
+    {
+        return;
+    }
+    putchar(*s);
+    _print_rev_recursion(s - 1);
+}
