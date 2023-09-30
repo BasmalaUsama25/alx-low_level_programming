35cc9c162f39dae62439a5a2b1da3c042dee9bed
basmala <ubasmala25@gmail.com>
2023-09-30 20:35:56 +0300

    function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ * _puts_recursion - prints a string
+ *
+ * @s: pointer
+ *
+ * Return: Always 1 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        putchar('\n');
+        return;
+    }
+    putchar(*s);
+    _puts_recursion(s + 1);
+}
