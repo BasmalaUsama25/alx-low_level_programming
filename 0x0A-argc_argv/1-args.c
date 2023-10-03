ecc2d0190f12e5d44b468890867859d0602e80d4
basmala <ubasmala25@gmail.com>
2023-10-03 15:00:16 +0300

    c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - prints the nomber of arguements
+ *
+ * @argc: the count of arguements 
+ *
+ * @argv: the value of argument
+ */ 
+
+int main(int argc, char *argv[])
+{
+	_put_char('%d\n',argc);
+}	
